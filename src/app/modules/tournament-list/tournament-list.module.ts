9c1e6b1a5bef3f355f3a8ddd9ba81f4bab162702
Jinbi Jin <jinbijin@jinbijin.nl>
2022-02-07 21:34:38 +0000

    Refactor list to cards

--- src/app/modules/tournament-list/tournament-list.module.ts
+++ src/app/modules/tournament-list/tournament-list.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { SharedModule } from '../shared/shared.module';
 import { TournamentListComponent } from './tournament-list.component';
 
 @NgModule({
   declarations: [TournamentListComponent],
-  imports: [CommonModule, SharedModule, MatListModule],
+  imports: [CommonModule, SharedModule, MatCardModule, MatProgressBarModule],
   exports: [TournamentListComponent],
 })
 export class TournamentListModule {}
