13119d24792b3b024604bb40f2fa31c75beb1378
Jinbi Jin <jinbijin@jinbijin.nl>
2022-02-08 19:35:57 +0000

    Fix UpdateNotificationService

--- src/app/core/pwa/update-notification.service.ts
+++ src/app/core/pwa/update-notification.service.ts
@@ -13,21 +13,23 @@ export class UpdateNotificationService {
         tap((event) => {
           switch (event.type) {
             case 'VERSION_READY':
-              const snackBarRef = this.snackBar.open(
-                'An update to a new version is ready to be installed.',
-                'Install'
-              );
+              const snackBarRef = this.snackBar.open('An update is ready to be installed.', 'Install');
               snackBarRef
                 .onAction()
                 .pipe(
                   switchMap(() => from(this.updates.activateUpdate())),
                   filter((x) => x),
-                  tap(() => this.snackBar.open('Update successful!', undefined, { duration: 5000 }))
+                  tap(() =>
+                    this.snackBar.open(
+                      'Update successful! Refresh the page to use the new version.',
+                      'Got it!'
+                    )
+                  )
                 )
                 .subscribe();
               break;
             case 'VERSION_DETECTED':
-              this.snackBar.open('An update to a new version is available and downloading...', undefined, {
+              this.snackBar.open('An update is available and downloading...', undefined, {
                 duration: 5000,
               });
               break;
